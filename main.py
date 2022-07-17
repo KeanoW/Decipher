b07358a62ee62017bebda02d9607f7c6438bd723
KeanoW <winterkeano02@gmail.com>
2022-07-17 10:50:27 +0200

    Fixed auther

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import cipher_functions as ciph_function
 import api
 # phrase = api.get_quote(type="q")
 dict_quotes_from_file = api.get_qoutes_from_file("Quotes.txt")
-phrase = dict_quotes_from_file["Seneca"]
+auther = "Seneca"
+phrase = dict_quotes_from_file[auther]
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
             "v", "w", "x", "y", "z", "*", "/", ",", "+", "&", "^"]
 
 phrase_alphabet = ciph_function.get_alphabet_of_phrase(ciph_function.remove_punctuation_marks(phrase))
 cipher_dict = dict(zip(phrase_alphabet, ciph_function.create_cipher_key_list(alphabet, phrase_alphabet)))
-print(cipher_dict)
+# print(cipher_dict)
 filtered_phrase_list = ciph_function.remove_punctuation_marks(phrase)
 ciphered_phrase = ciph_function.create_cipher_phrase(filtered_phrase_list, cipher_dict)
 count = 0
@@ -45,10 +46,10 @@ while(True):
 
         ciphered_phrase_str = "".join(map(str, modified_ciphered_phrase))
         print(ciphered_phrase_str)
-        print(modified_dict)
+        # print(modified_dict)
 
     if list(modified_dict.values()) == phrase_alphabet:
         print("Phrase deciphered!")
-        auther = api.get_quote(type="a")
+        # auther = api.get_quote(type="a")
         print(f"{phrase} - {auther}")
         break
